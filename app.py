08757bd888746301b269deb8fe2bf1d66a4ad57b
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-16 17:43:00 -0500

    Update app.py
    made a mistake and manage to resole it, clumsy me

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 #flask head
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
+api = Api(app)
+bcrypt = Bcrypt(app)
 cors = CORS(app)
-
-app.config['MONGODB_SETTING'] = {
-    'host': #Some URL for database
-}
-app.config["JWT_SECRET_KEY"] = #NowDisIsTheK3y
 
 #main system
 from db_model import initialize_db
 from urlkeys.routes import initialize_routes
-from flask_restful import Api
-from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-api = Api(app)
-bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': #URLfordatabase that you will never know
+}
+app.config["JWT_SECRET_KEY"] = #secret key that needs a env
 
 initialize_db(app)
 initialize_routes(api)
