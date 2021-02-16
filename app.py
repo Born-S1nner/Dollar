761a69cda2aa75c8a6b7681764acb25d943580f2
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-16 15:36:37 -0500

    Update app.py
    made an update for mongodb database, JWT, and bcrypt

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 #flask head
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Api
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 cors = CORS(app)
 
+app.config['MONGODB_SETTING'] = {
+    'host': #Some URL for database
+}
+app.config["JWT_SECRET_KEY"] = #NowDisIsTheK3y
+
 #main system
 from db_model import initialize_db
 from urlkeys.routes import initialize_routes
-
-initialize_db(app)
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+initialize_db(app)
 initialize_routes(api)
 
 if __name__ == "__main__":
