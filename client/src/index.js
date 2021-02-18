f4a154ba87e924db437f950543cf7d4b57bed268
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-18 13:54:33 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
 ReactDom.render(
     <React.StrictMode>
-        <h1>hello</h1>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
