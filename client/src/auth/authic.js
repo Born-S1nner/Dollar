3e37704c0ef1b777cd0da2dbf56167029e15c26e
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-23 14:33:13 -0500

    Update authic.js

--- client/src/auth/authic.js
+++ client/src/auth/authic.js
@@ -3,7 +3,7 @@ import {createAuthProvider} from 'react-token-auth'
 export const [useAuth, authFetch, login] = 
     createAuthProvider({
         accessTokenKey: 'access_token',
-        onUpdateToken: (token) => fetch('https://baree.herokuapp.com/user/refresh', {
+        onUpdateToken: (token) => fetch('https://dollardream.herokuapp.com/user/refresh', {
             method: "POST",
             body: token.access_token
         })
