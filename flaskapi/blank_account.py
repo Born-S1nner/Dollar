ed33d5d475862b928762a652d9d895267d4928c7
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-27 16:33:35 -0500

    Update blank_account.py

--- flaskapi/blank_account.py
+++ flaskapi/blank_account.py
@@ -1,14 +1,14 @@
 #Key routes and functions
 import datetime
+from db_model import CoinMember
 from flask_restful import Resource
 from flask import request
-from db_model import CoinMember
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 
 class Home(Resource):
     def get(self):
-        get = "Dollar_Dream"
-        return {"get": get}
+        get = "Dollar Dream"
+        return {"get" : get}   
 
 class LoginUser(Resource):
     def post(self):
