97b78b33ffa9252fd699169cb0a2c9d97db24da7
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-27 16:32:55 -0500

    Update app.py

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -13,12 +13,10 @@ CORS(app)
 
 #main system
 from db_model import initialize_db
-from blank_account import init_routes
+from ZFile.blank_account import init_routes
 
 URL = config('URL')
 KEY = config('KEY')
-print(URL)
-print(KEY)
 app.config['MONGODB_SETTINGS'] = {
     'host': "mongodb+srv://"+URL
 }
@@ -27,9 +25,11 @@ app.config["JWT_SECRET_KEY"] = KEY
 initialize_db(app)
 init_routes(api)
 
-@app.route('/')
-def hello():
-  return 'Hello World!'
+import sys
+import logging
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == "__main__":
   app.run(debug=True)
