fc3e817b45b16a0150fcb743537cb620c3665f24
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-01 14:00:49 -0500

    Update db_model.py

--- flaskapi/db_model.py
+++ flaskapi/db_model.py
@@ -9,13 +9,20 @@ def initialize_db (app):
 import mongoengine_goodjson as gj
 from flask_bcrypt import generate_password_hash, check_password_hash
 
+class BlogPoster(gj.Document):
+    blog = db.StringField(required=True, max_length=1000)
+    added_by = db.ReferenceField('CoinMember')
+
 class CoinMember(gj.Document):
     username = db.StringField(required=True, unique=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, unique=True, min_length=4)
+    blog = db.ListField(db.ReferenceField('BlogPoster', register_delete_rule=db.PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+CoinMember.register_delete_rule(BlogPoster, 'added_by', db.CASCADE)
