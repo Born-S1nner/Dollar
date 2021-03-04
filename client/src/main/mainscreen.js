93b74959d43cf4842d213026219c504eaa37b061
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-04 15:59:45 -0500

    Update mainscreen.js

--- client/src/main/mainscreen.js
+++ client/src/main/mainscreen.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
+import {useAuth} from '../auth/authic'
 
 export default function MainPage(){
   const [bloglines, blogChange] = useState('')
   fetch("https://dollardream.herokuapp.com/blog/public")
     .then(res => res.json())
     .then(data => blogChange(data.blog))
+  
+  const [inputLogged] = useAuth()
+  
   return (
     <div>
-      <h5>bloglines</h5>
+      <h4>bloglines</h4>
+      {!inputLogged? <h5>Please Log in to send a message...</h5>:<input/>}
       {bloglines}
     </div>
   )
