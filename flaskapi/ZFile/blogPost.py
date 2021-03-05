b20f1ddf5dedf1b33b91f298574a8f4ee5fc28be
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-05 14:34:28 -0500

    Update blogPost.py

--- flaskapi/ZFile/blogPost.py
+++ flaskapi/ZFile/blogPost.py
@@ -1,7 +1,7 @@
 #where the blog message gets postede
 from flask import request
 from flask_restful import Resource
-from db_model import BlogPoster, CoinMember
+from Zmodels.db_models import BlogPoster, CoinMember
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class BlogLine(Resource):
@@ -11,12 +11,12 @@ class BlogLine(Resource):
 
   @jwt_required
   def post(self):
-      user_id = get_jwt_identity()
-      body = request.get_json()
-      coin = CoinMember.objects.get(id=user_id)
-      blogpost = BlogPoster(**body, added_by=coin)
-      blogpost.save()
-      coin.update(push__blog=blogpost)
-      coin.save()
-      id = blogpost.id
-      return {"id": str(id)}, 200
+    coin_id = get_jwt_identity()
+    body = request.get_json()
+    coin = CoinMember.objects.get(id=coin_id)
+    message = BlogPoster(**body)
+    message.save()
+    id = message.id
+    print(id)
+    print(coin)
+    return{'id': str(id), 'coin_id': str(coin)}, 200
