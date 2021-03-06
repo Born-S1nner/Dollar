fc5dba0203575115d77479e689b0ecb6210e2ca3
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-06 14:48:08 -0500

    Update blogPost.py

--- flaskapi/ZFile/blogPost.py
+++ flaskapi/ZFile/blogPost.py
@@ -1,22 +1,29 @@
 #where the blog message gets postede
 from flask import request
 from flask_restful import Resource
-from Zmodels.db_models import BlogPoster, CoinMember
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from Zmodels.db_model import BlogPoster, CoinMember
+from Zmodels.error_model import InternalServerError, SchemaValidationError
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from mongoengine.errors import ValidationError, FieldDoesNotExist
 
 class BlogLine(Resource):
   def get(self):
     bloglist = BlogPoster.objects().to_json()
     return {"blog": bloglist}
 
-  @jwt_required
+  @jwt_required(optional=True)
   def post(self):
-    coin_id = get_jwt_identity()
-    body = request.get_json()
-    coin = CoinMember.objects.get(id=coin_id)
-    message = BlogPoster(**body)
-    message.save()
-    id = message.id
-    print(id)
-    print(coin)
-    return{'id': str(id), 'coin_id': str(coin)}, 200
+    try:
+      coin_id = get_jwt_identity()
+      body = request.get_json()
+      coin = CoinMember.objects.get(username=coin_id)
+      blog = BlogPoster(**body, added_by=coin)
+      blog.save()
+      coin.update(push__blogs=blog)
+      coin.save()
+      id = blog.id
+      return{"id": str(id)}, 200
+    except(FieldDoesNotExist, ValidationError):
+      raise SchemaValidationError
+    except Exception as e:
+      raise InternalServerError
