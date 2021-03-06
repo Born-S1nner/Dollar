13c8acffdce015671429a45c7b671dabea96d4f9
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-06 14:48:43 -0500

    Update app.py

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -4,23 +4,25 @@ from decouple import config
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from Zmodels.error_model import errors
 
 app = Flask(__name__)
-api = Api(app)
+KEY = config('KEY')
+app.config["JWT_SECRET_KEY"] = KEY
+
+api = Api(app, errors=errors)
 JWTManager(app)
 Bcrypt(app)
 CORS(app)
 
 #main system
-from db_model import initialize_db
+from Zmodels.db_model import initialize_db
 from ZFile.routes import init_routes
 
 URL = config('URL')
-KEY = config('KEY')
 app.config['MONGODB_SETTINGS'] = {
     'host': "mongodb+srv://"+URL
 }
-app.config["JWT_SECRET_KEY"] = KEY
 
 initialize_db(app)
 init_routes(api)
