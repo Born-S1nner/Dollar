2765fb9df6c92418dfe8e16de52fb582030908c5
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-06 15:10:15 -0500

    Update error_model.py

--- flaskapi/Zmodels/error_model.py
+++ flaskapi/Zmodels/error_model.py
@@ -1,3 +1,65 @@
+class InternalServerError(Exception):
+  pass
+
+class SchemaValidationError(Exception):
+  pass
+
+class UsernameAlreadyExist(Exception):
+  pass
+
+class UpdatingError(Exception):
+  pass
+
+class DeletingError(Exception):
+  pass
+
+class BlogDoesntExist(Exception):
+  pass
+
+class UnauthorizedError(Exception):
+  pass
+
+class BadTokenError(Exception):
+  pass
+
+class ExpiredTokenError(Exception):
+  pass
+
 errors = {
-  
+  "InternalServerError": {
+    "Message": "Something major is going down in the server",
+    "status": 500
+  },
+  "SchemaValidationError": {
+    "Message": "Request is missing requirments",
+    "status": 400
+  },
+  "UsernameAlreadyExist": {
+    "Message": "Username has been taken.",
+    "status": 400
+  },
+  "UpdatingError": {
+    "Message": "Prohibite from updating another user's blog",
+    "status": 403
+  },
+  "DeletingError": {
+    "Message": "Prohibite from deleting another user's blog",
+    "status": 403
+  },
+  "BlogDoesntExist": {
+    "Message": "Blog with given id doesn't exist",
+    "status": 400
+  },
+  "UnauthorizedError": {
+    "Message": "Invalid username/password",
+    "Status": 401
+  },
+  "BadTokenError": {
+    "Message": "Invalid Token",
+    "Status": 403
+  },
+  "ExpiredTokenError": {
+    "Message": "Token has expired, refresh the token",
+    "Status": 403
+  }
 }
