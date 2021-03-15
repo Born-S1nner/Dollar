849f10e4dcf53ea4e34a2777e9b5b7907e8f9919
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-15 15:53:44 -0400

    Update blogresults.js
    fifth attempt on this map

--- client/src/main/blogresults.js
+++ client/src/main/blogresults.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 export default function Mapblogs({bloglines}) {
-  const blogArr = bloglines.split(',');
   return (
-    blogArr.map((blog, index) => 
-      <li key={index}>
-        {blog}
-      </li>)
-    )
+    <div>
+      {bloglines.blog}
+    </div>
+  )
 }
