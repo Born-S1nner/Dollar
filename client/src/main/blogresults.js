c37699a40a361ce1b8c7048b695b4af6b83a3eca
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-16 16:37:06 -0400

    Update blogresults.js

--- client/src/main/blogresults.js
+++ client/src/main/blogresults.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function Mapblogs({bloglines}) {
-  return (
-    <div>
-      {bloglines.blog}
-    </div>
-  )
+class Mapblogs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogs: [],
+      loading: true
+    };
+  }
+  getBlogs() {
+    fetch("http://127.0.0.1:5000/blog/public")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          blogs: json,
+          loading: false
+        })
+      })
+  }
+  
+  componentDidMount() {
+    this.getBlogs();
+  }
+
+  render() {
+    return(
+      this.state.blogs.map(row => 
+        <div key={row.id}>
+          {row.blog}
+          {row.added_by.$oid}
+        </div>  
+      )
+    )
+  }
 }
+
+export default Mapblogs;
