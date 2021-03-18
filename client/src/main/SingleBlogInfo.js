d63b22a8f18056d5463a52a993832a4929adc477
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-18 16:01:27 -0400

    Update SingleBlogInfo.js

--- client/src/main/SingleBlogInfo.js
+++ client/src/main/SingleBlogInfo.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-export default function SingleBlog() {
-  return (
-    <div>
-      <p>
-        heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
-      </p>
-      <p>
-      heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
-      </p>
-      <p>
-      heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
-      </p>
-      <Link to='/home'>Return</Link>
-    </div>
-  )
+class SingleBlog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogline: []
+    }
+  }
+
+  getBlogLine({id}) {
+    fetch(`http://127.0.0.1:5000/blog/public/${id}`)
+  }
+
+  displayBlogs = (blogline) => {
+    return(
+      blogline.map(row => 
+        <div key={row.id} className="blogRow" >
+          <h5 className='blog_head'>{row.added_by}</h5>
+          <p className='blog_blog'>{row.blog}</p>
+          <button value={row.id} onClick={this.UpdateBlogRow} className="blog_button">Edit</button>
+          <button className="blog_button">Remove</button>
+        </div>
+      )
+    )
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.displayBlogs(this.state.blogline)}</div>
+        <Link to='/home'>Return</Link>
+      </div>
+    )
+  }
 }
+
+export default SingleBlog
