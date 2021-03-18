8a6cb100ef433d027c4164b9fc7eb3bc4a772645
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-18 14:59:58 -0400

    Create SingleBlogInfo.js

---
+++ client/src/main/SingleBlogInfo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function SingleBlog() {
+  return (
+    <div>
+      <p>
+        heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
+      </p>
+      <p>
+      heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
+      </p>
+      <p>
+      heawtoinebgasrvgnaeoirvgnjsisisisisisisiebv
+      </p>
+      <Link to='/home'>Return</Link>
+    </div>
+  )
+}
