f3a6e3623acd102e512c40df19dc68ea0b5fbb0a
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-19 19:42:12 -0400

    Update SingleBlogInfo.js

--- client/src/main/SingleBlogInfo.js
+++ client/src/main/SingleBlogInfo.js
@@ -9,8 +9,16 @@ class SingleBlog extends Component {
     }
   }
 
-  getBlogLine({id}) {
-    fetch(`http://127.0.0.1:5000/blog/public/${id}`)
+  getBlogLine() {
+    fetch("http://127.0.0.1:5000/blog/private/605112fca3793a90b95e7993")
+      .then(res => res.json())
+      .then(json =>{ 
+        this.setState({
+          blogline: json
+        })
+      })
+      .catch(err=> console.log(err))
+    
   }
 
   displayBlogs = (blogline) => {
@@ -25,10 +33,15 @@ class SingleBlog extends Component {
       )
     )
   }
+
+  componentDidMount() {
+    this.getBlogLine()
+  }
+
   render() {
     return (
       <div>
-        <div>{this.displayBlogs(this.state.blogline)}</div>
+        {this.displayBlogs(this.state.blogline)}
         <Link to='/home'>Return</Link>
       </div>
     )
