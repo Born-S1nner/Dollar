beb4bebf0e5316149a772d80135867e830b6e544
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-24 15:02:47 -0400

    Update SingleBlogInfo.js

--- client/src/main/blog/SingleBlogInfo.js
+++ client/src/main/blog/SingleBlogInfo.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
-//import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 class SingleBlog extends Component {
 
   render() {
 //<Link to='/home'>Return</Link>
     return (
-      this.props.blogline.map(row => 
+      <div>
+      {this.props.blogline.map(row => 
         <div key={row.id} className="blogRow" >
           <p className='blog_blog'>{row.blog}</p>
           <button value={row.id} onClick={this.UpdateBlogRow} className="blog_button">Edit</button>
           <button className="blog_button">Remove</button>
         </div>
-      )
+      )}
+      <Link to='/home'>Return</Link>
+      </div>
     )
   }
 }
