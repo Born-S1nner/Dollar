48310ff1231bcf9372ca1a071444adbd308b641b
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-24 13:56:47 -0400

    Update SingleBlogInfo.js

--- client/src/main/blog/SingleBlogInfo.js
+++ client/src/main/blog/SingleBlogInfo.js
@@ -1,48 +1,18 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 class SingleBlog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blogline: []
-    }
-  }
-  getBlogLine() {
-    let idOutlet = this.props.identity
-    console.log(idOutlet)
-    fetch("http://127.0.0.1:5000/blog/private/"+idOutlet)
-      .then(res => res.json())
-      .then(json =>{ 
-        this.setState({
-          blogline: json
-        })
-      })
-      .catch(err=> console.log(err))
-  }
 
-  displayBlogs = (blogline) => {
-    return(
-      blogline.map(row => 
+  render() {
+//<Link to='/home'>Return</Link>
+    return (
+      this.props.blogline.map(row => 
         <div key={row.id} className="blogRow" >
           <p className='blog_blog'>{row.blog}</p>
           <button value={row.id} onClick={this.UpdateBlogRow} className="blog_button">Edit</button>
           <button className="blog_button">Remove</button>
         </div>
       )
-    )
-  }
-
-  componentDidMount() {
-    this.getBlogLine()
-  }
-
-  render() {
-    return (
-      <div>
-        {this.displayBlogs(this.state.blogline)}
-        <Link to='/home'>Return</Link>
-      </div>
     )
   }
 }
