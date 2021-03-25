4617b586db18205a5a8476e7dd90fb03635f5659
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-25 16:52:15 -0400

    Update blank_account.py

--- flaskapi/ZFile/blank_account.py
+++ flaskapi/ZFile/blank_account.py
@@ -1,6 +1,7 @@
 #Key routes and functions
 import datetime
-from Zmodels.db_model import CoinMember, Refressher
+
+from Zmodels.db_model import CoinMember
 from flask_restful import Resource
 from flask import request
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
@@ -21,12 +22,10 @@ class LoginUser(Resource):
             return {'Error': "Incorrect Email or Password"}, 401
 
         access_expires = datetime.timedelta(hours=23)
-        refresh_expires = datetime.timedelta(minutes=30)
+        refresh_expires = datetime.timedelta(hours=23)
 
         access_token = create_access_token(identity=username, expires_delta=access_expires, fresh=True)
         refresh_token = create_refresh_token(identity=username, expires_delta=refresh_expires)
-        RToken = Refressher(refresh=refresh_token, username=username)
-        RToken.save()
         ret = {
             "access_token": access_token,
             "refresh_token": refresh_token
@@ -54,19 +53,12 @@ class NewUser(Resource):
 class refresh(Resource):
     @jwt_required(refresh=True)
     def post(self):
-        body = request.get_json()
-        username = body.get('username')
         current_user = get_jwt_identity()
-        userName = Refressher.objects.get(username=current_user)
-        userKey = userName['username']
-        if username == userKey:
-            new_token = create_access_token(identity=current_user)
-            ret = {
-                'access_token': new_token
-            }
-            return ret, 200
-        else:
-            return {'Error': 'unable to connect, Logout and Log back in.'}, 403
+        new_token = create_access_token(identity=current_user)
+        ret = {
+            'access_token': new_token
+        }
+        return ret, 200
 
 class protection(Resource):
     @jwt_required()
