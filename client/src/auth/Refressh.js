c4a90154ccfc18df924f545d5c841187aa371a8e
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-25 17:26:06 -0400

    Update Refressh.js

--- client/src/auth/Refressh.js
+++ client/src/auth/Refressh.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { login } from '../auth/authic'
 
-export default function Refressh() {
+export default function Refressh({setToken, refresh}) {
   let onRefreshClick = (e) => {
     e.preventDefault()
-    console.log("REEEEE")
+    fetch("http://127.0.0.1:5000/user/refresh", {
+      method: "POST",
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${refresh}`,
+        'Accept': 'application/json'
+      }
+    })
+    .then(token => {
+      if (token.access_token) {
+        login(token)
+        setToken(token.access_token)
+      } else {
+        alert("You need to log out to reset")
+      }
+    })
   }
   return (
     <div>
