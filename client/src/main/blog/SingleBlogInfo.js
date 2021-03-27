27ab94dd6f41edafaad869275944ce1df6242430
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-03-27 18:33:06 -0400

    Update SingleBlogInfo.js

--- client/src/main/blog/SingleBlogInfo.js
+++ client/src/main/blog/SingleBlogInfo.js
@@ -3,13 +3,17 @@ import {Link} from 'react-router-dom'
 
 class SingleBlog extends Component {
 
-  UpdateBlogRow(e) {
-    let id = e.currentTarget.value
-    console.log(id)
-  }
   DeleteBlogRow(e){
     let id = e.currentTarget.value
-    console.log(id)
+    fetch("http://127.0.0.1:5000/blog/private/"+id, {
+      method: 'DELETE',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json' 
+      }
+    })
+    fetch("http://127.0.0.1:5000/blog/public")
+      .then(res => res.json())
   }
 
   render() {
@@ -18,19 +22,12 @@ class SingleBlog extends Component {
       {this.props.blogline.map(row => 
         <div key={row.id} className="blogRow" >
           <p className='blog_blog'>{row.blog}</p>
-          <button 
-            value={row.id.$oid}
-            onClick={this.UpdateBlogRow} 
-            className="blog_button"
-          >
-            Edit
-          </button>
           <button
             value={row.id.$oid}
-            onClick={this.UpdateBlogRow} 
+            onClick={this.DeleteBlogRow} 
             className="blog_button"
           >
-            Remove
+            <Link className="LinkStyle" to='/home'>Remove</Link>
           </button>
           <button className="blog_button">
             <Link className="LinkStyle" to='/home'>Return</Link>
